bbb7f099708d298b6278e6ed4a55579187822771
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-30 20:00:50 +0800

    added checker interface

---
+++ Github.Updater/IChecker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Github.Updater
+{
+    interface IChecker
+    {
+        bool? Check(string repository, string currentVersion);
+    }
+}
