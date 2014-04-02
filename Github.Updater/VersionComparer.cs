c05cf1d39a497307157bbb8308c40ac60e3341a9
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-02 22:48:38 +0800

    version comparer implementation

---
+++ Github.Updater/VersionComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Github.Updater
+{
+    class VersionComparer: IComparer<string>
+    {
+        public int Compare(string alpha, string bravo)
+        {
+            return 0;
+        }
+    }
+}
