8df720a1fa63184938c2f5ba8fb98c6c443e2dfb
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-04 14:14:33 +0800

    version object normalizer

---
+++ Github.Updater/Version.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Github.Updater
+{
+    public class Version
+    {
+        public int Major { get; set; }
+
+        public int Minor { get; set; }
+
+        public int Patch { get; set; }
+
+        public int Build { get; set; }
+
+        public string Tag { get; set; }
+
+        public Version(string version)
+        {
+            Match match = Regex.Match(version, "/^([0-9]+)\\.([0-9]+)\\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*))?(?:\\+[0-9A-Za-z-]+)?$/");
+            
+        }
+    }
+}
