f8c389b0bec49634bd076cbc769b406a3b6647a0
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-06 20:22:59 +0800

    added unit testing project

---
+++ UnitTester/VersionTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UpdaterVersion = Github.Updater.Version;
+
+namespace UnitTester
+{
+    [TestClass]
+    public class VersionTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            UpdaterVersion version = new UpdaterVersion("1.0.0");
+        }
+    }
+}
