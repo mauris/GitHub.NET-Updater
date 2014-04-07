544459a69ddd77fb7517516e36466957aca22a68
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-07 21:10:55 +0800

    removed unused hash and regex

--- Github.Updater/Version.cs
+++ Github.Updater/Version.cs
@@ -18,18 +18,15 @@ namespace Github.Updater
 
         public string Tag { get; set; }
 
-        public string Hash { get; set; }
-
         public Version(string version)
         {
-            Match match = Regex.Match(version, @"^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\\+[0-9A-Za-z-]+)?$");
+            Match match = Regex.Match(version, @"^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+))?$");
             if (match.Success && match.Groups.Count == 5)
             {
                 Major = Convert.ToInt32(match.Groups[1].Value);
                 Minor = Convert.ToInt32(match.Groups[2].Value);
                 Patch = Convert.ToInt32(match.Groups[3].Value);
                 Tag = match.Groups[4].Value;
-                Hash = match.Groups[5].Value;
             }
             else
             {
