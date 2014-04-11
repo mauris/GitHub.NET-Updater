cecd802c10221656e3ff74b1bf24b4d4ba160fe4
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-12 00:14:48 +0800

    updated version compare

--- Github.Updater/VersionComparer.cs
+++ Github.Updater/VersionComparer.cs
@@ -5,10 +5,34 @@ using System.Text;
 
 namespace Github.Updater
 {
-    class VersionComparer: IComparer<string>
+    class VersionComparer: IComparer<Version>
     {
-        public int Compare(string alpha, string bravo)
+        public int Compare(Version alpha, Version bravo)
         {
+            if (alpha.Major < bravo.Major)
+            {
+                return -1;
+            }
+            else if (alpha.Major > bravo.Major)
+            {
+                return 1;
+            }
+            if (alpha.Minor < bravo.Minor)
+            {
+                return -1;
+            }
+            else if (alpha.Minor > bravo.Minor)
+            {
+                return 1;
+            }
+            if (alpha.Patch < bravo.Patch)
+            {
+                return -1;
+            }
+            else if (alpha.Patch > bravo.Patch)
+            {
+                return 1;
+            }
             return 0;
         }
     }
